128f684c2ee3b6959e9dfb828779f6d5c7b7e219
Jasper5143 <jamieson.jasper@lindisfarne.school.nz>
2024-08-22 14:30:52 +1200

    Gladiator's glory 0.1

---
+++ Scripts/game.gd
@@ -0,0 +1,19 @@
+extends Node2D
+var wave = 0
+var kills = 0
+
+# Called when the node enters the scene tree for the first time.
+
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+
+
+
+		
+func _process(delta):
+	pass
+
+
