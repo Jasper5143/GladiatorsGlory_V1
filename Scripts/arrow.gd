839c1d9ecf76f3cccea7bfb9e84d4dd494936fd3
Jasper5143 <jamieson.jasper@lindisfarne.school.nz>
2024-08-30 14:45:43 +1200

    G.G 1.2.1
    At this point: skeletons and arrows are spawning, but do not contribute to kills

--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -1,8 +1,22 @@
 extends Node2D
-
+@onready var Player = get_tree().get_first_node_in_group("Player")
+@onready var sword = preload("res://Prefabs/sword.tscn")
 @export var speed = 10
 var direction = Vector2.ZERO
 
 func _process(delta):
 	position += direction * speed 
 	
+
+
+
+
+func _on_area_2d_body_entered(body):
+	queue_free()
+	if body.name == 'Player':
+		get_tree().change_scene_to_file("res://Prefabs/death_screen.tscn")
+
+
+func _on_area_2d_area_entered(area):
+	if area.name == 'sword':
+		queue_free()
